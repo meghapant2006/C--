d987170953945594ae94c74c261b6243771900e7
Megha pant <pantmegha195@gmail.com>
2025-09-20 23:55:43 +0530

    CPP

--- C++ Series/Operator/types_op.cpp
+++ C++ Series/Operator/types_op.cpp
@@ -3,20 +3,26 @@ using namespace std;
 int main()
 {
 
-    // 1. Assignmenrt Operators...
+    // 1. Arithmetic Operators...
+    int a = 10, b = 5;
+    cout << (a + b) << "\n"; // 15
+    cout << (a - b) << "\n"; // 5
+    cout << (a * b) << "\n"; // 50
+
+    // 2. Assignmenrt Operators...
     int x = 10;
     x += 5;            // x = x + 5
     cout << x << "\n"; // 15
 
-    // 2. Comparison Operators...
+    // 3. Comparison Operators...
     int a = 10;
     int b = 5;
-    cout << (a > b) << "\n";   // 1 (true)
+    cout << (a > b) << "\n"; // 1 (true)
 
-    // 3. Logical Operators...
+    // 4. Logical Operators...
     int p = 10;
     int q = 5;
     cout << (p > 5 && q < 10) << "\n"; // 1 (true)
     cout << (p > 5 || q > 10) << "\n"; // 1 (true)
-    cout << !(p < 5) << "\n";         // 1 (true)
+    cout << !(p < 5) << "\n";          // 1 (true)
 }
