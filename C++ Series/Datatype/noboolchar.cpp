1a8646a1fc3f6ae6ada4e2bebc2dc30a85069073
Megha pant <pantmegha195@gmail.com>
2025-09-20 23:33:48 +0530

    CPP

---
+++ C++ Series/Datatype/noboolchar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+
+    // 1 INTEGER....................
+    int myNum = 1000;
+    cout << myNum;
+
+    // 2 FLOAT....................
+    float myNum = 5.75;
+    cout << myNum;
+
+    // 3 DOUBLE....................
+    double myNum = 19.99;
+    cout << myNum;
+
+    // 4 BOOLEAN....................
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun; // Outputs 1 (true)
+    cout << isFishTasty; // Outputs 0 (false)
+
+    // 5 CHARACTER....................
+    char myGrade = 'A';
+    cout << myGrade;
+   
+
+    return 0;
+}
