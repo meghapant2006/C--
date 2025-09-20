7fb176677059805b2de0f2e80f73c5e05c48a61e
Megha pant <pantmegha195@gmail.com>
2025-09-21 00:06:18 +0530

    CPP

--- C++ Series/Datatype/str.cpp
+++ C++ Series/Datatype/str.cpp
@@ -10,21 +10,25 @@ int main()
     string lastName = "Pant";
     string fullName = firstName + lastName;
     cout << "Full Name is: " << fullName << "\n";
-    
 
-//   string access....................
 
+
+    //  String Length......................
     string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     cout << "The length of the txt string is: " << txt.length() << "\n";
 
+
+    //   string access....................
     string myString = "Hello";
     cout << myString[0] << "\n";
 
     string myStringg = "megha";
     myStringg[4] = 'A';
     cout << myStringg << "\n";
-    
 
+    // Omitting namespace..............
+    std::string greeting = "Hello World!";
+    std::cout << greeting << "\n";
 
     return 0;
 }
